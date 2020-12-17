15ede4703789facadc8eecbdaf5fba0f26b87b54
janhavi544 <janhavisingh544@gmail.com>
2020-12-17 22:49:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quickwhatsapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.quickwhatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("WA Direct Message");
+    }
+}
