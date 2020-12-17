11f3542adcd4041f964c2cf6600c5f7613b5170f
janhavi544 <janhavisingh544@gmail.com>
2020-12-17 23:51:15 +0530

    front end done

--- app/src/main/java/com/example/quickwhatsapp/MainActivity.java
+++ app/src/main/java/com/example/quickwhatsapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.quickwhatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("WA Direct Message");
+    }
+    public void onCountryPickerClick(View view)
+    {
+        CountryCodePicker ccp;
+        ccp = findViewById(R.id.ccp);
+        String selInitial=ccp.getDefaultCountryCode();
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                //Alert.showMessage(RegistrationActivity.this, ccp.getSelectedCountryCodeWithPlus());
+                String selected_country_code = ccp.getSelectedCountryCodeWithPlus();
+
+            }
+        });
     }
 }
