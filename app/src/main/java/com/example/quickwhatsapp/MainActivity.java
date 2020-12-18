83bbb5d9f6ebfd4aad5a81592c2636e2abe6be59
janhavi544 <janhavisingh544@gmail.com>
2020-12-18 18:00:53 +0530

    back end done

--- app/src/main/java/com/example/quickwhatsapp/MainActivity.java
+++ app/src/main/java/com/example/quickwhatsapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.quickwhatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,10 +13,17 @@ import android.widget.EditText;
 
 import com.hbb20.CountryCodePicker;
 
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
     EditText phone;
     EditText message;
-    CountryCodePicker ccp;
+    String countryCode="91";
+    String URLEncoded;
+    String messageToBeSend;
+    String phnNumber;
+    boolean msg=false;
+    CountryCodePicker countryCodePicker;
     Button send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         message=(EditText)findViewById(R.id.editTextTextMultiLine);
         send=(Button)findViewById(R.id.sendButton);
         send.setVisibility(View.INVISIBLE);
+        countryCodePicker = findViewById(R.id.ccp);
         phone.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -34,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             if(s!=null && s.length()==10)
-                send.setVisibility(View.VISIBLE);
+            {
+                phnNumber=countryCode+phone.getText().toString();
+                msg=true;
+            }
             }
 
             @Override
@@ -42,26 +55,54 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        send.setOnClickListener(new View.OnClickListener() {
+        message.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                String mess=message.getText().toString();
-                String phn=ccp.getSelectedCountryCodeWithPlus()+phone.getText().toString();
-                //do stuff
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(s!=null && s.length()>0)
+            {
+                send.setVisibility(View.VISIBLE);
+                messageToBeSend=message.getText().toString();
+                msg=true;
+            }
+            else
+                msg=false;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
+        if(msg==true)
+        {
+            phnNumber=countryCode+phone.getText().toString();
+            messageToBeSend=message.getText().toString();
+        }
     }
     public void onCountryPickerClick(View view)
     {
-        ccp = findViewById(R.id.ccp);
-        String selInitial=ccp.getDefaultCountryCode();
-        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+        countryCodePicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
             @Override
             public void onCountrySelected() {
                 //Alert.showMessage(RegistrationActivity.this, ccp.getSelectedCountryCodeWithPlus());
-                String selected_country_code = ccp.getSelectedCountryCodeWithPlus();
-               ///do stuff
+                countryCode = countryCodePicker.getSelectedCountryCode();
             }
         });
+    }
+    public void sendButtonClicked(View view)
+    {
+        try {
+            String query = URLEncoder.encode(messageToBeSend, "utf-8");
+            URLEncoded = "https://wa.me/" + phnNumber + "?text=" + query;
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(URLEncoded));
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
